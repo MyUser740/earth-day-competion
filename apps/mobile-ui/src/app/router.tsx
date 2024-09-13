f340d2bc01da4c1a1776a16c74e57cb9a17d9b1e
MyUser740 <axel020207@gmail.com>
2024-09-13 12:36:52 +0700

    Add news route

--- apps/mobile-ui/src/app/router.tsx
+++ apps/mobile-ui/src/app/router.tsx
@@ -6,6 +6,7 @@ import UnAuthHomePage from './pages/unauth-home.page';
 import LoginPage from './pages/login.page';
 import HomePage from './pages/home.page';
 import DashboardPage from './pages/dashboard.page';
+import NewsPage from './pages/news.page';
 
 export default function Router() {
   return (
@@ -16,6 +17,7 @@ export default function Router() {
           <Route path="/login" render={() => <LoginPage />} />
           <Route path="/home" render={() => <HomePage />} />
           <Route path="/dashboard/:locid" render={() => <DashboardPage />} />
+          <Route path="/news" render={() => <NewsPage />} />
           <Redirect exact from="/" to="/unauth" />
         </IonRouterOutlet>
       </IonReactRouter>
