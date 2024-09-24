d601177b82b067edf8af0f8c5eea6c8a294e2345
WhiteWolf <73521401+MyUser740@users.noreply.github.com>
2024-09-25 05:49:05 +0700

    Update app controller

--- apps/backend-service/src/app/app.controller.ts
+++ apps/backend-service/src/app/app.controller.ts
@@ -1,13 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { LlmServiceService } from './llm-service/llm-service.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly llmService: LlmServiceService,
+  ) { }
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Post('/llm-chat')
+  async llmChat(
+    @Body('prompt') prompt: string | null,
+    @Body('histories')
+    histories: Array<{ role: 'assistant' | 'user'; msg: string }> | null,
+  ) {
+    if (!prompt) throw new BadRequestException('prompt is required');
+    else if (!histories) throw new BadRequestException('histories is required');
+
+    return await this.llmService.createCompletion({ prompt, histories });
   }
 }
